fff0e3d5041dc39468b6a650e06ea669d36d62d1
rakesh b <rakeshbsirvi46@gmail.com>
2025-11-09 23:12:14 +0530

    use ollama api with authentication

--- src/models/ollama_api.py
+++ src/models/ollama_api.py
@@ -1,7 +1,10 @@
 # we will create a class to interact with the Ollama API for language model operations with ollama models and langchain-ollama integration
 
+import os
+import base64
 import requests
 from langchain_ollama import ChatOllama
+from langchain_ollama.embeddings import OllamaEmbeddings
 from typing import List, Dict
 
 class OllamaAPI:
@@ -50,3 +53,50 @@ class OllamaAPI:
             return " ".join([str(item) for item in content])
         else:
             return str(content)
+        
+class OllamaAPINew:
+
+    def __init__(self, base_url: str, model_name: str, username: str = None, password: str = None, should_authenticate: bool = False):
+        self.model_name = model_name
+        self.base_url = base_url
+        self.username = username
+        self.password = password
+        self.text_embedding_model_name = "nomic-embed-text"
+        
+        if should_authenticate and (not username or not password):
+            raise ValueError("Username and password must be provided for authentication.")
+        
+        if should_authenticate:
+            self.auth_token = base64.b64encode(f"{username}:{password}".encode()).decode()
+            self.headers = {
+                "Authorization": f"Basic {self.auth_token}"
+            }
+            self.llm = ChatOllama(model=model_name, base_url=base_url, 
+            temperature=0.7, client_kwargs={ "headers": self.headers })
+
+            self.embeddings = OllamaEmbeddings(model=self.text_embedding_model_name, base_url=base_url, client_kwargs={ "headers" : self.headers})
+
+        else:
+            self.llm = ChatOllama(model=model_name, base_url=base_url, temperature=0.7)
+            self.embeddings = OllamaEmbeddings(model=self.text_embedding_model_name, base_url=base_url)
+        
+
+    def get_models(self) -> List[Dict]:
+        """Return list of available Ollama models.
+            [{
+                "id": "gemma:2b",
+                "object": "model",
+                "created": 1757934271,
+                "owned_by": "library"
+            }]
+        """
+        response = requests.get(f"{self.base_url}/v1/models", headers=self.headers if hasattr(self, 'headers') else {})
+
+        if response.status_code == 200:
+            data = response.json().get("data", [])
+
+            if data:
+                # filter object type model
+                models = [model for model in data if model.get("object") == "model"]
+                return models
+        return []
